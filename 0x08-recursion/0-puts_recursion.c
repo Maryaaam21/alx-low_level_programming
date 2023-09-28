8721adb6cb327dc965c14875fe412f8d440acec7
Maryaaam21 <maryamhassan1821@gmail.com>
2023-09-28 01:45:55 -0700

    0-puts_recursion task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function similar to puts
+ * @s: the char to input
+ * @*s: the string
+ * Return: always 0 on success
+ */
+
+void _puts_recursion(char *s)
+
+{
+if(*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+}
