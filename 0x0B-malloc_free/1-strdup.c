a4a077e0c56de327e76c8b47d3b0f49f745c27f1
Maryaaam21 <maryamhassan1821@gmail.com>
2023-10-22 03:32:58 -0700

    corrections on tasks

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,6 +6,7 @@
  * _strdup - function that duplicate to new memory space location
  * @str: char
  * Return: 0
+ */
 
 char *_strdup(char *str)
 
@@ -15,18 +16,15 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 	return (NULL);
-
-		i = 0;
+	i = 0;
 	while (str[i] != '\0')
-		i++;
-
+	i++;
 	aaa = malloc(sizeof(char) * (i + 1));
 
 	if (aaa == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (r = 0; str[r]; r++)
 	aaa[r] = str[r];
-
 	return (aaa);
 }
