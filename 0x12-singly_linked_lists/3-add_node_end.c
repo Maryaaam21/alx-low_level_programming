a92400b908e5c5f29a54385dcf670ae47aad1b65
Maryaaam21 <maryamhassan1821@gmail.com>
2023-10-23 01:38:22 -0700

    task 0 to 101 including main.h, _putchar.c and readme files

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node to the end of the list
+ * @head: represents the head of the node
+ * @str: the string
+ * Return: the address of the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+
+{
+	list_t *newestnode, *tmp;
+	unsigned int i, count = 0;
+
+	newestnode = malloc(sizeof(list_t));
+	if (newestnode == NULL)
+		return (NULL);
+
+	newestnode->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	newestnode->len = count;
+	newestnode->next = NULL;
+	tmp = *head;
+
+	if (tmp == NULL)
+		*head = newestnode;
+	else
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = newestnode;
+	}
+	return (*head);
+}
