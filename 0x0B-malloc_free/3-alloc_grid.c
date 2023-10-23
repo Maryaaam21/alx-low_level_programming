4646f30a9a90c15d5a0663fe033916e44f2ca952
Maryaaam21 <maryamhassan1821@gmail.com>
2023-10-23 09:16:49 -0700

    finishing up more tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,15 +27,15 @@ int **alloc_grid(int width, int height)
 	if (mee[x] == NULL)
 {
 	for (; x >= 0; x--)
-		free(mee[x]);
-		free(mee);
+	free(mee[x]);
+	free(mee);
 	return (NULL);
 }
 }
 	for (x = 0; x < height; x++)
 {
-		for (y = 0; y < width; y++)
-		mee[x][y] = 0;
+	for (y = 0; y < width; y++)
+	mee[x][y] = 0;
 }
 	return (mee);
 }
