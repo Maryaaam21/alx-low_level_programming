b307959f6a3367b83d75bca7c68355963b83c8d4
Maryaaam21 <maryamhassan1821@gmail.com>
2023-10-24 20:00:55 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,32 +17,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int m = 0, o = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1])
+while (s1 && s1[len1])
 		len1++;
-	while (s2 && s2[len2])
+while (s2 && s2[len2])
 		len2++;
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+if (n < len2)
+s = malloc(sizeof(char) * (len1 + n + 1));
+else
+	s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (!s)
-		return (NULL);
+if (!s)
+	return (NULL);
 
-	while (m < len1)
+while (m < len1)
 	{
-		s[m] = s1[m];
-		m++;
+	s[m] = s1[m];
+	m++;
 	}
 
-	while (n < len2 && m < (len1 + n))
-		s[m++] = s2[o++];
+while (n < len2 && m < (len1 + n))
+	s[m++] = s2[o++];
 
-	while (n >= len2 && m < (len1 + len2))
-		s[m++] = s2[o++];
+while (n >= len2 && m < (len1 + len2))
+	s[m++] = s2[o++];
 
-	s[m] = NULL;
-
+s[m] = NULL;
 	return (s);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,14 +17,14 @@ int *array_range(int min, int max)
 if (min > max)
 	return (NULL);
 
-	size = max - min + 1;
-	ptr = malloc(sizeof(int) * size);
+size = max - min + 1;
+ptr = malloc(sizeof(int) * size);
 
 if (ptr == NULL)
 	return (NULL);
 
 for (m = 0; min <= max; m++)
-	ptr[m] = min++;
+ptr[m] = min++;
 
 return (ptr);
 }
