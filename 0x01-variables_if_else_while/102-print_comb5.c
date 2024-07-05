de34b7d65ca34fd649b8ece3c0b07b13a58cab13
Maryaaam21 <maryamhassan1821@gmail.com>
2024-07-05 13:02:36 -0700

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 99; i++)
 	{
-	for (j = i; j <= 99; j++)
+	for (j = i; j + 1 <= 99; j++)
 	{
 		putchar(i / 10 + '0');
 		putchar(i % 10 + '0');
@@ -23,7 +23,7 @@ int main(void)
 		putchar(j / 10 + '0');
 		putchar(j % 10 + '0');
 
-	if (i != 98 || j != 99)
+	if (i != 98 && j != 99)
 	{
 		putchar(',');
 		putchar(' ');
