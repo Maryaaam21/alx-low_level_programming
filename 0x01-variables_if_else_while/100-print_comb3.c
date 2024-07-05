4e4a110dcce3485c8f78bb93cb848bc307745e64
Maryaaam21 <maryamhassan1821@gmail.com>
2024-07-05 10:00:48 -0700

    task 9, corrections

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints all
+ * possible combinations of two digits.
+ *
+ * Return: always 0, on success
+ */
+
+int main(void)
+{
+	int i; int j;
+
+	for (i = 0; i 
